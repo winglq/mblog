79b0e8002ea7e84c892f56d5aba4cbf68e0b59d5
Liu Qing <winglq@gmail.com>
2017-10-02 12:38:27 +0800

    load images by jquery
    Signed-off-by: Liu Qing <winglq@gmail.com>

--- mblog/templates/functions.js
+++ mblog/templates/functions.js
@@ -18,6 +18,7 @@ function replace(tag_id, resp, process_fn){
 function load_replace(endpoint, tag_id, process_fn=default_process_fn){
     load(endpoint, function cb(resp){
         replace(tag_id, resp, process_fn)
+        images_loaded()
         change_img_to_responsive()
         })
 }
@@ -29,9 +30,22 @@ function list_to_link_process_fn(resp){
     })
     return html
 }
+
 function change_img_to_responsive(){
     imgs = $("img").each(function(){
         $(this).addClass("img-responsive")
+    })
+}
+
+function images_loaded(){
+    var dfds = [];
+    $('img[src!=""]').each(function(){
+        var dfd = $.Deferred();
+        dfds.push(dfd);
+        var img = new Image();
+        img.onload = function(){dfd.resolve();}
+        img.onerror = function(){dfd.resolve();}
+        img.src = this.src;
     })
 }
 </script>

6fddc6bcdebaec6110a1deae17ee39bc939217d5
Liu Qing <liuqing@chinac.com>
2017-11-21 16:14:36 +0800

    add access log
    Signed-off-by: Liu Qing <liuqing@chinac.com>

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,3 +1,4 @@
 source ../bin/activate
 export PYTHONPATH=$(pwd)
-gunicorn --paste etc/mblog/mblog.ini --config etc/mblog/gunicorn.conf.py
+mkdir /var/log/mblog
+gunicorn --paste etc/mblog/mblog.ini --config etc/mblog/gunicorn.conf.py --access-logfile /var/log/mblog/access.log
